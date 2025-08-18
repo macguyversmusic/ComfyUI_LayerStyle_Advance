4f9bb0acff62d0cc0bd246eb911c85ab5e0e80df
MacGuyver-DEV <89907172+macguyversmusic@users.noreply.github.com>
2025-08-18 13:04:51 -0500

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -4,21 +4,21 @@ repair_dependency_txt="$(dirname "$0")/repair_dependency_list.txt"
 requirements_txt="$(dirname "$0")/requirements.txt"
 
 echo "Install whl..."
-python -m pip install ./whl/docopt-0.6.2-py2.py3-none-any.whl
-python -m pip install ./whl/hydra_core-1.3.2-py3-none-any.whl
+pip install ./whl/docopt-0.6.2-py2.py3-none-any.whl
+pip install ./whl/hydra_core-1.3.2-py3-none-any.whl
 
 echo "Install requirements.txt..."
 while IFS= read -r line; do
-    python -m pip install "$line"
+    pip install "$line"
 done < "$requirements_txt"
 
 echo
 echo "Fixing Dependency Package..."
-python -m pip uninstall -y onnxruntime
-python -m pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless opencv-contrib-python-headless
+pip uninstall -y onnxruntime
+pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless opencv-contrib-python-headless
 
 while IFS= read -r line; do
-    python -m pip install "$line"
+    pip install "$line"
 done < "$repair_dependency_txt"
 
 echo
